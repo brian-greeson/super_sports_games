1668434cd0207b8fe18118f17cdd6faf86c12141
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-01-30 14:50:31 -0700

    Update event class to pass knows_standard_deviation_of_ages test

--- lib/event.rb
+++ lib/event.rb
@@ -19,4 +19,21 @@ class Event
     (@ages.sum.to_f / @ages.length).round(2)
   end
 
+  def standard_deviation_age
+    mean = self.average_age
+
+    ages_minus_mean = []
+    ages.each {|age| ages_minus_mean << age - mean }
+
+    ages_minus_mean_squared = []
+    ages_minus_mean.each {|age_minus_mean| ages_minus_mean_squared << age_minus_mean * age_minus_mean}
+
+    squares_divided_by_length = ages_minus_mean_squared.sum / @ages.length
+
+
+    standard_devation = Math.sqrt(squares_divided_by_length)
+    standard_devation.round(2)
+
+  end
+
 end
